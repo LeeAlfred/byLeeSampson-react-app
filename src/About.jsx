84aa6ffc171d90b1a9e29179ca64c9f2f5833025
Lee Sampson <emailme@leesampson.co.nz>
2021-07-01 16:35:34 +0200

    add favicon, update <About> content

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function About() {
     return <div> <p> Welcome to my evolving project website for displaying my React web development skills.</p>
-    <p>The content is from my Leadership Cartoons originallyposted on my Linkedin feed.</p>
+    <p>The content is from my Leadership Cartoons originally posted on my Linkedin feed.</p>
     </div>
 }
 
